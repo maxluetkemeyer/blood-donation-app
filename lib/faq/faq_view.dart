ba9a7fba5261b5c55244755680eb63c27749ab19
tschmit2 <tschmit2@uni-muenster.de>
2021-09-25 11:58:15 +0200

    Changes to the Title

--- lib/faq/faq_view.dart
+++ lib/faq/faq_view.dart
@@ -52,7 +52,10 @@ class _HomePageState extends State<HomePage> {
                   )),
               body: Container(
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                child: Text("[Anwendung der App]"),
+                child: Text(
+                  "[Anwendung der App]",
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
             ),
             ExpansionPanelRadio(
@@ -67,7 +70,9 @@ class _HomePageState extends State<HomePage> {
               body: Container(
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
                 child: Text(
-                    "Blutspenden sichern die Versorgung von Patienten während Operationen und Therapien, somit rettet gespendetes Blut Leben."),
+                  "Blutspenden sichern die Versorgung von Patienten während Operationen und Therapien, somit rettet gespendetes Blut Leben.",
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
             ),
             ExpansionPanelRadio(
@@ -97,7 +102,9 @@ class _HomePageState extends State<HomePage> {
               body: Container(
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
                 child: Text(
-                    "Damit Sie mit einen sicheren Gefühl zu Ihrer ersten Blutspende gehen können, legen wir besonderen Wert darauf, dass ein Erstspender umfassend informiert wird, Fragen beantworten wir gerne im Arztgespräch"),
+                  "Damit Sie mit einen sicheren Gefühl zu Ihrer ersten Blutspende gehen können, legen wir besonderen Wert darauf, dass ein Erstspender umfassend informiert wird, Fragen beantworten wir gerne im Arztgespräch",
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
             ),
             ExpansionPanelRadio(
@@ -112,7 +119,9 @@ class _HomePageState extends State<HomePage> {
               body: Container(
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
                 child: Text(
-                    "Sie müssen zwischen 18 und 68 Jahre alt sein, jedoch bei Aufnahme in unsere Spenderdatei als Erstspender nicht älter als 60 Jahre alt sein.\nSie müssen völlig gesund sein und mindestens 50 kg wiegen.\nIn den vergangenen 4 Wochen darf keine Magen-Darm-Infektion, kein fieberhafter Infekt und keine andere Infektionskrankheit vorgelegen haben.\nSie dürfen keine bestehenden Grunderkrankungen (wie z. B. Diabetes,  Bluthochdruck, Herzkrankheiten, Autoimmunerkrankungen, Epilepsie usw.) haben.\nSie dürfen nicht regelmäßig Medikamente (außer Hormonpräparate) einnehmen.\nWährend Ihrer Schwangerschaft und der Stillzeit dürfen Sie kein Blut spenden.\nSie dürfen keine Tumorkrankheiten haben.\nAuslandsreisen (auch innerhalb von Europa) können unter Umständen einen befristeten Ausschluß von der Blutspende nach sich ziehen. Bitte kontaktieren Sie uns gerne für weitere Informationen unter unserer Hotline 0251-8358000. Sie dürfen sich in dem Zeitraum zwischen 1980 und 1996 nicht länger als sechs Monate in Großbritannien aufgehalten haben (auch Kurzaufenthalte zusammengerechnet).\nSie dürfen in den letzten vier Monaten nicht gepierct oder tätowiert worden sein.\nSie dürfen kein Piercing im Mund- oder Intimbereich haben.\nSie dürfen in Ihrem Leben nicht an einer Malaria erkrankt sein.\nSie dürfen keine Operationen, Magen- oder Darmspiegelungen in den letzten vier Monaten gehabt haben\nBei neuen Sexualpartnern erfolgt eine zeitlich begrenzte Rückstellung von der Blutspende für 4 Monate.\nVor eine Blutspende sollten Sie ausreichend gegessen und getrunken haben."),
+                  "Sie müssen zwischen 18 und 68 Jahre alt sein, jedoch bei Aufnahme in unsere Spenderdatei als Erstspender nicht älter als 60 Jahre alt sein.\nSie müssen völlig gesund sein und mindestens 50 kg wiegen.\nIn den vergangenen 4 Wochen darf keine Magen-Darm-Infektion, kein fieberhafter Infekt und keine andere Infektionskrankheit vorgelegen haben.\nSie dürfen keine bestehenden Grunderkrankungen (wie z. B. Diabetes,  Bluthochdruck, Herzkrankheiten, Autoimmunerkrankungen, Epilepsie usw.) haben.\nSie dürfen nicht regelmäßig Medikamente (außer Hormonpräparate) einnehmen.\nWährend Ihrer Schwangerschaft und der Stillzeit dürfen Sie kein Blut spenden.\nSie dürfen keine Tumorkrankheiten haben.\nAuslandsreisen (auch innerhalb von Europa) können unter Umständen einen befristeten Ausschluß von der Blutspende nach sich ziehen. Bitte kontaktieren Sie uns gerne für weitere Informationen unter unserer Hotline 0251-8358000. Sie dürfen sich in dem Zeitraum zwischen 1980 und 1996 nicht länger als sechs Monate in Großbritannien aufgehalten haben (auch Kurzaufenthalte zusammengerechnet).\nSie dürfen in den letzten vier Monaten nicht gepierct oder tätowiert worden sein.\nSie dürfen kein Piercing im Mund- oder Intimbereich haben.\nSie dürfen in Ihrem Leben nicht an einer Malaria erkrankt sein.\nSie dürfen keine Operationen, Magen- oder Darmspiegelungen in den letzten vier Monaten gehabt haben\nBei neuen Sexualpartnern erfolgt eine zeitlich begrenzte Rückstellung von der Blutspende für 4 Monate.\nVor eine Blutspende sollten Sie ausreichend gegessen und getrunken haben.",
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
             ),
             ExpansionPanelRadio(
@@ -127,7 +136,9 @@ class _HomePageState extends State<HomePage> {
               body: Container(
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
                 child: Text(
-                    "Nach der Anmeldung zu der Blutspende muss vor Ort ein Fragebogen ausgefüllt werden. Vor der Spende wird die Konzentration des Hämoglobins im Blut geprüft. Nach einem Arztgespräch, bei dem der Gesundheitszustand der Spender:innen abgefragt wird, kann Blut gespendet werden. Eine Blutspende benötigt maximal 12 Minuten. Die Einzelspende hat ein Volumen von ca 450 ml Blut. Nach der Spende sollten noch ca. 30 Minuten im Wartezimmer verbracht werden, um Kreislaufreaktionen zu vermeiden. Dazu gibt es Getränke, einen Imbiss und eine Aufwandsentschädigung."),
+                  "Nach der Anmeldung zu der Blutspende muss vor Ort ein Fragebogen ausgefüllt werden. Vor der Spende wird die Konzentration des Hämoglobins im Blut geprüft. Nach einem Arztgespräch, bei dem der Gesundheitszustand der Spender:innen abgefragt wird, kann Blut gespendet werden. Eine Blutspende benötigt maximal 12 Minuten. Die Einzelspende hat ein Volumen von ca 450 ml Blut. Nach der Spende sollten noch ca. 30 Minuten im Wartezimmer verbracht werden, um Kreislaufreaktionen zu vermeiden. Dazu gibt es Getränke, einen Imbiss und eine Aufwandsentschädigung.",
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
             ),
             ExpansionPanelRadio(
@@ -156,7 +167,9 @@ class _HomePageState extends State<HomePage> {
               body: Container(
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
                 child: Text(
-                    "Die Erstspende wird mit 10,00 € vergütet, ab der 2. liegt der Betrag bei 20,00 €, bei der 3. im Kalenderjahr einmalig erhält der Spender einmalig 30,00 € für eine der drei Spenden.\nDer Betrag kann auch auf Wunsch gespendet werden."),
+                  "Die Erstspende wird mit 10,00 € vergütet, ab der 2. liegt der Betrag bei 20,00 €, bei der 3. im Kalenderjahr einmalig erhält der Spender einmalig 30,00 € für eine der drei Spenden.\nDer Betrag kann auch auf Wunsch gespendet werden.",
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
             ),
             ExpansionPanelRadio(
@@ -171,7 +184,9 @@ class _HomePageState extends State<HomePage> {
               body: Container(
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
                 child: Text(
-                    "Für die Blutentnahme wird ausschließlich steriles Einwegmaterial verwendet. Eine Ansteckungsgefahr ist für Sie somit ausgeschlossen. Die Blutspende selbst wird im Allgemeinen gut vertragen; trotzdem sollten Sie sich am Spendetag nicht vermehrter körperlicher Belastung aussetzen. "),
+                  "Für die Blutentnahme wird ausschließlich steriles Einwegmaterial verwendet. Eine Ansteckungsgefahr ist für Sie somit ausgeschlossen. Die Blutspende selbst wird im Allgemeinen gut vertragen; trotzdem sollten Sie sich am Spendetag nicht vermehrter körperlicher Belastung aussetzen. ",
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
             ),
             ExpansionPanelRadio(
@@ -186,7 +201,9 @@ class _HomePageState extends State<HomePage> {
               body: Container(
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
                 child: Text(
-                    "Frauen dürfen alle zwölf Wochen, jedoch maximal vier Mal im Jahr, zur Vollblutspende gehen. Männer dürfen alle acht Wochen, jedoch maximal sechs Mal im Jahr, zur Vollblutspende gehen."),
+                  "Frauen dürfen alle zwölf Wochen, jedoch maximal vier Mal im Jahr, zur Vollblutspende gehen. Männer dürfen alle acht Wochen, jedoch maximal sechs Mal im Jahr, zur Vollblutspende gehen.",
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
             ),
             ExpansionPanelRadio(
@@ -200,7 +217,10 @@ class _HomePageState extends State<HomePage> {
                   )),
               body: Container(
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                child: Text("[Begründung]"),
+                child: Text(
+                  "[Begründung]",
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
             ),
           ].toList(),
