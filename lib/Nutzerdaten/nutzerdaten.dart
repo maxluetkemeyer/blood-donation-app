56bd5f5e2305baab5a73fb29017a50de6b050d23
Lukas <l.janeba@gmx.de>
2021-09-25 02:00:08 +0200

    update to nutzerdaten

--- lib/Nutzerdaten/nutzerdaten.dart
+++ lib/Nutzerdaten/nutzerdaten.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Nutzerdaten extends StatelessWidget {
+  /* setPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('number', 1);
+    int numbers = (prefs.getInt('number') ?? 2);
+    print(numbers);
+  }
+
+  getPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int numbers = (prefs.getInt('number') ?? 2);
+    print(numbers);
+  }
+*/
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +40,7 @@ class Nutzerdaten extends StatelessWidget {
               SizedBox(
                 width: 300,
                 height: 30,
-                child: const Text("Name",
+                child: Text("Name",
                     textAlign: TextAlign.left, style: TextStyle(fontSize: 16)),
               ),
             ]),
@@ -67,7 +82,7 @@ class Nutzerdaten extends StatelessWidget {
                   child: const Text("Ihre letzten Blutspenden",
                       style: TextStyle(fontSize: 16))),
             ]),
-
+            /*
             SizedBox(
               child: TableCalendar(
                 firstDay: DateTime.utc(2021, 9, 01),
@@ -76,7 +91,73 @@ class Nutzerdaten extends StatelessWidget {
                 calendarFormat: CalendarFormat.month,
               ),
             ),
+            */
+            SizedBox(height: 500, child: Calendar())
           ]),
         )));
   }
+}
+
+class Calendar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SfCalendar(
+      view: CalendarView.month,
+      dataSource: MeetingDataSource(_getDataSource()),
+      monthViewSettings: MonthViewSettings(
+          appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
+    ));
+  }
+}
+
+List<Meeting> _getDataSource() {
+  final List<Meeting> meetings = <Meeting>[];
+  meetings.add(Meeting("Blutspende", DateTime.utc(2021, 9, 17),
+      DateTime.utc(2021, 9, 17, 2), Color(0xFF0F8644), true));
+  meetings.add(Meeting("Blutspende", DateTime.utc(2021, 9, 18),
+      DateTime.utc(2021, 9, 18, 2), Color(0xFF0F8644), false));
+
+  return meetings;
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return appointments![index].from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return appointments![index].to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return appointments![index].eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return appointments![index].background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].isAllDay;
+  }
+}
+
+class Meeting {
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+  String eventName;
+  DateTime from;
+  DateTime to;
+  Color background;
+  bool isAllDay;
 }
