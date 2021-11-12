f436e478813a940baa84cf0b33787050e627bb08
Dominik Eitner <dominikeitner@gmail.com>
2021-11-12 12:30:38 +0100

    Adding singlechildscrollView to booking start view

--- lib/appointment_booking/bookingstart_view.dart
+++ lib/appointment_booking/bookingstart_view.dart
@@ -16,50 +16,52 @@ class BookingStartView extends StatelessWidget {
         left: 10,
         right: 10,
       ),
-      child: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(
-              top: 60,
-              bottom: 40,
-            ),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Blutspendetermin",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w600,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(
+                top: 60,
+                bottom: 40,
+              ),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Blutspendetermin",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
             ),
-          ),
-          const Text(
-            "Wenn Sie „Termin buchen“ auswählen, können Sie einfach und unkompliziert einen Blutspendetermin am Universitätsklinikum Münster buchen. \nAls ersten Schritt müssen Sie dafür ein paar Fragen beantworten, damit mögliche Ausschlusskriterien für Ihre Spende festgestellt werden können.\nHaben Sie keine der Ausschlusskriterien erfüllt, können Sie auch schon einen Termin und eine Uhrzeit für Ihren Blutspendetermin auswählen.\nDann müssen Sie nur noch einmal Ihre Daten überprüfen - und schon haben Sie einen Blutspendetermin!",
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          ),
-          const SizedBox(height: 20),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 12),
-            width: double.infinity,
-            child: CupertinoButton.filled(
-              child: const Text("Termin buchen"),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const QuestionsView()),
+            const Text(
+              "Wenn Sie „Termin buchen“ auswählen, können Sie einfach und unkompliziert einen Blutspendetermin am Universitätsklinikum Münster buchen. \nAls ersten Schritt müssen Sie dafür ein paar Fragen beantworten, damit mögliche Ausschlusskriterien für Ihre Spende festgestellt werden können.\nHaben Sie keine der Ausschlusskriterien erfüllt, können Sie auch schon einen Termin und eine Uhrzeit für Ihren Blutspendetermin auswählen.\nDann müssen Sie nur noch einmal Ihre Daten überprüfen - und schon haben Sie einen Blutspendetermin!",
+              style: TextStyle(
+                fontSize: 20,
               ),
             ),
-          ),
-          const SizedBox(height: 40),
-          // ignore: sized_box_for_whitespace
-          Container(
-            width: width * 0.6,
-            height: height * 0.3,
-            child: const Placeholder(),
-          ),
-        ],
+            const SizedBox(height: 20),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 12),
+              width: double.infinity,
+              child: CupertinoButton.filled(
+                child: const Text("Termin buchen"),
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const QuestionsView()),
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+            // ignore: sized_box_for_whitespace
+            Container(
+              width: width * 0.6,
+              height: height * 0.3,
+              child: const Placeholder(),
+            ),
+          ],
+        ),
       ),
     );
   }
