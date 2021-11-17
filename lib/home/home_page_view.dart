ac5941b6028ba7e6c8d47d72155e93428ae4ff64
Max Lütkemeyer <max.luetkemeyer@uni-muenster.de>
2021-11-17 11:29:11 +0100

    Update home_page_view.dart

--- lib/home/home_page_view.dart
+++ lib/home/home_page_view.dart
@@ -8,61 +8,65 @@ class HomePageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          width: double.infinity,
-          height: 250,
-          child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(4)),
-            ),
-            child: CarouselSlider(
-              options: CarouselOptions(height: 400.0),
-              items: [1, 2, 3, 4].map((i) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                        decoration:
-                            const BoxDecoration(color: Color(0xff003866)),
-                        child: Image(
-                          image: AssetImage(
-                            './assets/images/image_$i.png',
-                          ),
-                          fit: BoxFit.cover,
-                        ));
-                  },
-                );
-              }).toList(),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(
-            top: 20,
-          ),
-          child: Text(
-            AppLocalizations.of(context)!.homePageViewInfo,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              fontSize: 18,
+    double width = MediaQuery.of(context).size.width;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: 250,
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(4)),
+              ),
+              child: CarouselSlider(
+                options: CarouselOptions(height: 400.0),
+                items: [1, 2, 3, 4].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration:
+                              const BoxDecoration(color: Color(0xff003866)),
+                          child: Image(
+                            image: AssetImage(
+                              './assets/images/image_$i.png',
+                            ),
+                            fit: BoxFit.cover,
+                          ));
+                    },
+                  );
+                }).toList(),
+              ),
             ),
           ),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(
-            left: 80,
-            right: 80,
-            top: 30,
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 20,
+            ),
+            child: Text(
+              AppLocalizations.of(context)!.homePageViewInfo,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 18,
+              ),
+            ),
           ),
-          child: Image(
-            image: AssetImage(
-                "./assets/images/Universitätsklinikum_Münster_Logo.png"),
-          ),
-        )
-      ],
+          Padding(
+            padding: EdgeInsets.only(
+              left: 0.3*width,
+              right: 0.3*width,
+              top: 30,
+            ),
+            child: const Image(
+              image: AssetImage(
+                  "./assets/images/Universitätsklinikum_Münster_Logo.png"),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
