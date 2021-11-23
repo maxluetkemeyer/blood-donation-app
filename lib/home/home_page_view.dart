674a2eb60112a3c641ede44fcda43260db818bd4
Dominik Eitner <dominikeitner@gmail.com>
2021-11-23 15:43:05 +0100

    Implement changed Image Carousel

--- lib/home/home_page_view.dart
+++ lib/home/home_page_view.dart
@@ -1,15 +1,13 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'build_carousel.dart';
 
 class HomePageView extends StatelessWidget {
   const HomePageView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-
     return SingleChildScrollView(
       child: Column(
         children: [
@@ -17,50 +15,30 @@ class HomePageView extends StatelessWidget {
             width: double.infinity,
             height: 250,
             child: Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(4)),
-              ),
-              child: CarouselSlider(
-                options: CarouselOptions(height: 400.0),
-                items: [1, 2, 3, 4].map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                          width: MediaQuery.of(context).size.width,
-                          margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                          decoration:
-                              const BoxDecoration(color: Color(0xff003866)),
-                          child: Image(
-                            image: AssetImage(
-                              './assets/images/image_$i.png',
-                            ),
-                            fit: BoxFit.cover,
-                          ));
-                    },
-                  );
-                }).toList(),
-              ),
-            ),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(4)),
+                ),
+                child: buildImageCarousel()),
           ),
-          Padding(
-            padding: const EdgeInsets.only(
+          const Padding(
+            padding: EdgeInsets.only(
               top: 20,
             ),
             child: Text(
-              AppLocalizations.of(context)!.homePageViewInfo,
+              "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
               textAlign: TextAlign.center,
-              style: const TextStyle(
+              style: TextStyle(
                 fontSize: 18,
               ),
             ),
           ),
-          Padding(
+          const Padding(
             padding: EdgeInsets.only(
-              left: 0.3*width,
-              right: 0.3*width,
+              left: 80,
+              right: 80,
               top: 30,
             ),
-            child: const Image(
+            child: Image(
               image: AssetImage(
                   "./assets/images/Universitätsklinikum_Münster_Logo.png"),
             ),
