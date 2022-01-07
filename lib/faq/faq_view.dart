66c6cc7502154f5688a3f8ba4905f2dfecd10b2e
Max Lütkemeyer <max.luetkemeyer@uni-muenster.de>
2022-01-08 00:47:45 +0100

    faq with flutter web view
    has to be discussed

--- lib/faq/faq_view.dart
+++ lib/faq/faq_view.dart
@@ -1,15 +1,22 @@
+import 'dart:convert';
+
 import 'package:blooddonation/faq/html.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 import 'faq_question_widget.dart';
 import '../models/faq_question_model.dart';
 import 'faq_question_list.dart';
 
 ///This Widget is used, to show the user the faq interface.
-class FaqView extends StatelessWidget {
+class FaqView extends StatefulWidget {
   const FaqView({Key? key}) : super(key: key);
 
+  @override
+  State<FaqView> createState() => _FaqViewState();
+}
+
+class _FaqViewState extends State<FaqView> {
   ///Build method to build the faq page, utilizing the [faqQuestionList] which is a list of [FaqQuestion]'s.
   ///
   ///Returns a [Widget] tree.
@@ -30,14 +37,18 @@ class FaqView extends StatelessWidget {
                 canTapOnHeader: true,
                 headerBuilder: (_, isExpanded) => Container(
                   padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                  child: const Text(
-                    "Test",
-                  ),
+                  child: const Text("Test"),
                 ),
                 body: Container(
                   padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                  child: Html(
-                    data: htmlData,
+                  //width: 100,
+                  height: 500,
+                  child: WebView(
+                    javascriptMode: JavascriptMode.disabled,
+                    onWebViewCreated: (controller) {
+                      controller.loadHtmlString(htmlData);
+                    },
+                    zoomEnabled: false,
                   ),
                 ),
               ),
